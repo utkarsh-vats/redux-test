0412a38c902421fc17e725c1efb6d34b5fae5738
utkarsh-vats <utkarsh.vats.2k@gmail.com>
2021-01-18 21:07:42 +0530

    minor changes

--- src/store/actions/fetchUsers.js
+++ src/store/actions/fetchUsers.js
@@ -1,11 +1,11 @@
 import { FETCH_USERS } from "../constants.js";
 
-// const url = "https://reqres.in/api/users";
-let id = 1;
-const url = `https://reqres.in/api/users?page=${id}`;
+const url = `https://reqres.in/api/users?page=`;
 
 const fetchUsers = async (dispatch) => {
-    await fetch(url)
+    let id = Math.floor(Math.random() * 2) + 1;
+    let fetchURL = `${url}${id}`;
+    await fetch(fetchURL)
     .then(res => res.json())
     .then(res => dispatch({type: FETCH_USERS, payload: res.data}));
 };
